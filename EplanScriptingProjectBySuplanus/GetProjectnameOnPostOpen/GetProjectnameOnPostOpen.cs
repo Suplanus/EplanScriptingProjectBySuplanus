0cf01609c6731e7a91d5efb94ffbb61af3b80376
Johann Weiher <info@suplanus.de>
2014-06-05 12:57:51 +0200

    GetProjectnameOnPostOpen added

---
+++ EplanScriptingProjectBySuplanus/GetProjectnameOnPostOpen/GetProjectnameOnPostOpen.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Scripting;
+
+public class GetProjectnameOnPostOpen
+{
+    [DeclareEventHandler("Eplan.EplApi.OnPostOpenProject")]
+    public void MyEventHandlerFunction(IEventParameter iEventParameter)
+    {
+        try
+        {
+            EventParameterString oEventParameterString = new EventParameterString(iEventParameter);
+            MessageBox.Show("Projekt öffnen:\n" + oEventParameterString.String, "OnPostOpenProject");
+
+        }
+        catch (System.InvalidCastException exc)
+        {
+            MessageBox.Show(exc.Message, "Fehler");
+        }
+    }
+}
