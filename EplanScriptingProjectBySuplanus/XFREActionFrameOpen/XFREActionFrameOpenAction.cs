c44c6edd412b261b2352df1123119b0d7291e024
Johann Weiher <info@suplanus.de>
2014-07-17 12:28:07 +0200

    XFREActionFrameOpen added

---
+++ EplanScriptingProjectBySuplanus/XFREActionFrameOpen/XFREActionFrameOpenAction.cs
@@ -0,0 +1,14 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Scripting;
+
+class XFREActionFrameOpenAction
+{
+    [Start]
+    public void XFREActionFrameOpenActionFunction()
+    {
+        ActionCallingContext acc = new ActionCallingContext();
+        acc.AddParameter("NewFrame", "1");
+        acc.SetStrings(new string[] { @"\\Path\To\Frame\NameOfFile.fn1" });
+        new CommandLineInterpreter().Execute("XFREActionFrameOpen", acc);
+    }
+}
