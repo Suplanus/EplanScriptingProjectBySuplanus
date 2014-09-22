11b4ced3b388e13c61de8a1c9cb513339b5d4e98
Johann Weiher <info@suplanus.de>
2014-09-22 08:34:57 +0200

    TranslateLanguages added

---
+++ EplanScriptingProjectBySuplanus/TranslateLanguages/TranslateLanguages.cs
@@ -0,0 +1,15 @@
+﻿using Eplan.EplApi.Base;
+using Eplan.EplApi.Scripting;
+using System;
+using System.Windows.Forms;
+
+public class TranslateLanguages
+{
+    [Start]
+    public void Execute()
+    {
+        Settings setting = new Settings();
+        string stringSetting = setting.GetStringSetting("USER.TRANSLATE.TRANSLATE_LANGUAGES", 0);
+        MessageBox.Show(stringSetting.Replace(";", Environment.NewLine));
+    }
+}
