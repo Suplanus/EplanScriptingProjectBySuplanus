f5660011c0e9139a81dbe88807281734c3696108
Johann Weiher <info@suplanus.de>
2015-07-29 09:49:46 +0200

    GetCurrentScriptPath added

---
+++ EplanScriptingProjectBySuplanus/GetCurrentScriptPath/GetCurrentScriptPath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using Eplan.EplApi.Scripting;
+
+namespace EplanScriptingProjectBySuplanus.GetCurrentScriptPath
+{
+    public class GetCurrentScriptPath
+    {
+        [Start]
+        public void GetActualScriptPathAbfragen()
+        {
+            MessageBox.Show("Current script-path:" + Environment.NewLine + GetPath(),
+                "GetCurrentScriptPath", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public string GetPath()
+        {
+            Eplan.EplApi.Base.Settings oSettings = new Eplan.EplApi.Base.Settings();
+            string path= oSettings.GetExpandedStringSetting(
+                "USER.FileSelection.1.PermamentSelection.FolderName", 0);
+            return path;
+        }
+    }
+}
