1fdd8b7bbfd5830f5cfe9708bafe8084baac5290
Johann Weiher <info@suplanus.de>
2015-08-05 07:30:39 +0200

    GetProjectProperty modified

--- EplanScriptingProjectBySuplanus/GetProjectProperty/GetProjectProperty.cs
+++ EplanScriptingProjectBySuplanus/GetProjectProperty/GetProjectProperty.cs
@@ -1,55 +1,68 @@
-﻿using System.Windows.Forms;
-using System.Xml;
+﻿/* Usage
+	private static string GetProjectProperty(string id, string index)
+	{
+		string value = null;
+		ActionCallingContext actionCallingContext = new ActionCallingContext();
+		actionCallingContext.AddParameter("id", id);
+		actionCallingContext.AddParameter("index", index);
+		new CommandLineInterpreter().Execute("GetProjectProperty", actionCallingContext);
+		actionCallingContext.GetParameter("value", ref value);
+		return value;
+	}
+*/
+
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Eplan.EplApi.ApplicationFramework;
 using Eplan.EplApi.Base;
 using Eplan.EplApi.Scripting;
 
-public class GetProjectProperty
+namespace EplanScriptingProjectBySuplanus.GetProjectProperty
 {
-    [DeclareAction("GetProjectProperty")]
-    public void Function(int ID)
+    class GetProjectProperty
     {
-        string filename =
-            PathMap.SubstitutePath("$(PROJECTPATH)" + @"\"
-            + "Projectinfo.xml");
-
-        string PropertyValue = ReadXml(filename,ID);
-
-        MessageBox.Show(
-            "Eigenschaftwert von " + ID + ":\n" +
-            PropertyValue,
-            "Information",
-            MessageBoxButtons.OK,
-            MessageBoxIcon.Information
-            );
-
-        return;
-    }
-
-    private static string ReadXml(string filename, int id)
-    {
-        string lastVersion = string.Empty;
-        XmlTextReader reader = new XmlTextReader(filename);
-        while (reader.Read())
+        [DeclareAction("GetProjectProperty")]
+        public void Action(string id, string index, out string value)
         {
-            if (reader.HasAttributes)
-            {
-                while (reader.MoveToNextAttribute())
-                {
-                    if (reader.Name == "id")
-                    {
-                        if (reader.Value == id.ToString())
-                        {
-                            return lastVersion = reader.ReadString();
-                        }
-                    }
-                }
-            }
-        }
+            string pathTemplate = Path.Combine(PathMap.SubstitutePath("$(MD_SCRIPTS)"),
+                "GetProjectProperty", "GetProjectProperty_Template.xml");
+            string pathScheme = Path.Combine(PathMap.SubstitutePath("$(MD_SCRIPTS)"),
+                "GetProjectProperty", "GetProjectProperty_Scheme.xml");
 
-        return lastVersion;
+            try
+            {
+                // Set scheme
+                string content = File.ReadAllText(pathTemplate);
+                content = content.Replace("GetProjectProperty_ID", id);
+                content = content.Replace("GetProjectProperty_INDEX", index);
+                File.WriteAllText(pathScheme, content);
+
+                new Settings().ReadSettings(pathScheme);
+
+                string pathOutput = Path.Combine(
+                    PathMap.SubstitutePath("$(MD_SCRIPTS)"), "GetProjectProperty",
+                    "GetProjectProperty_Output.txt");
+
+                // Export
+                ActionCallingContext actionCallingContext = new ActionCallingContext();
+                actionCallingContext.AddParameter("configscheme", "GetProjectProperty");
+                actionCallingContext.AddParameter("destinationfile", pathOutput);
+                actionCallingContext.AddParameter("language", "de_DE");
+                new CommandLineInterpreter().Execute("label", actionCallingContext);
+
+                // Read
+                value = File.ReadAllLines(pathOutput)[0];
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "GetProjectProperty", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                value = "[Error]";
+            }
+
+        }
     }
+
 
 }
-
-
-
