a078917e39e8f740dff36941f1e616c5e2f67dff
Suplanus <info@suplanus.de>
2018-02-16 09:15:39 +0100

    Eventlogger

--- EplanScriptingProjectBySuplanus/Eventlogger/Eventlogger.cs
+++ EplanScriptingProjectBySuplanus/Eventlogger/Eventlogger.cs
@@ -1,27 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Eplan.EplApi.ApplicationFramework;
 using Eplan.EplApi.Scripting;
-public class SimpleEventHandler
+using EventHandler = Eplan.EplApi.ApplicationFramework.EventHandler;
+
+public class EventLogger
 {
-    [Start]
-    [DeclareEventHandler("onActionEnd.String.*")]
-    public long MyEventHandlerFunction2(IEventParameter iEventParameter)
+  EventHandler eventHandler = new EventHandler();
+
+  [DeclareRegister]
+  public void Register()
+  {
+    eventHandler.SetEvent("*");
+    EventHandlerNameFunction eventHandlerNameFunction = Event;
+    eventHandler.EplanNameEvent += eventHandlerNameFunction;
+  }
+
+  [DeclareUnregister]
+  public void UnRegister()
+  {
+    eventHandler.Dispose();
+  }
+
+  private void Event(IEventParameter eventParameter, string eventName)
+  {
+    // Check Blacklist
+    List<string> blackList = new List<string>
     {
-        try
-        {
-            EventParameterString oEventParameterString = new EventParameterString(iEventParameter);
-            String strActionName = oEventParameterString.String;
-            StreamWriter sw;
-            FileInfo fi = new FileInfo(@"C:\Test\Events.txt");
-            sw = fi.AppendText();
-            sw.WriteLine("oCLI.Execute(\"onActionEnd.String.{0}\");", strActionName);
-            sw.Close();
-        }
-        catch (InvalidCastException exc)
-        {
-            System.Windows.Forms.MessageBox.Show("Parameter error: " + exc.Message, "MyEventHandler");
-        }
-        return 0;
+      "onIdle.Bool.App",
+      "onLastIdle.Bool.App",
+      "onTimer.UInt.App"
+    };
+
+    if (blackList.Contains(eventName))
+    {
+      return;
     }
+
+    // Get Parameter
+    string parameter;
+    try
+    {
+      EventParameterString eventParameterString = new EventParameterString(eventParameter);
+      parameter = eventParameterString.String;
+    }
+    catch
+    {
+      parameter = string.Empty;
+    }
+
+    // Write log
+    FileInfo fileInfo = new FileInfo(@"C:\Test\Events.txt");
+    using (StreamWriter streamWriter = fileInfo.AppendText())
+    {
+      string line = string.Format("{1}{0}{2}{0}{3}",
+        "\t",
+        DateTime.Now.ToString("HH:mm:ss"),
+        eventName,
+        parameter);
+
+      streamWriter.WriteLine(line);
+    }
+  }
 }
